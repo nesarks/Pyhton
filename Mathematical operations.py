451f2f355838df557c118ac862cfe15899d72da0
Nesar K.S <96533558+nesarks@users.noreply.github.com>
2023-04-25 18:35:12 +0530

    mathematical operations

---
+++ Mathematical operations.py
@@ -0,0 +1,42 @@
+# Mathematical Operations
+
+# Addition
+a = 10
+b = 20
+print(a + b) # 30
+
+# Subtraction
+a = 10
+b = 20
+print(b - a) # 10
+
+# Multiplication
+a = 10
+b = 20
+print(a * b) # 200
+
+# Division
+a = 10
+b = 20
+print(b / a) # 2.0
+
+# Modulo
+a = 10
+b = 20
+print(b % a) # 0
+
+# Power
+a = 10
+b = 2
+print(a ** b) # 100
+
+# Operator Precedence
+a = 10
+b = 20
+c = 30
+
+# Without Parentheses
+print(a + b * c) # 210
+
+# With Parentheses
+print((a + b) * c) # 900

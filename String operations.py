c6d3895371eff47078f00988305defc84ea362c4
Nesar K.S <96533558+nesarks@users.noreply.github.com>
2023-05-11 19:51:31 +0530

    String operations

---
+++ String operations.py
@@ -0,0 +1,12 @@
+string = "Hello World!"
+for char in string:
+    print(char)
+print("First 5 characters:", string[:5])
+print("Last 5 characters:", string[-5:])
+string1 = "Hello"
+string2 = "World"
+string3 = string1 + " " + string2
+print("Concatenated string:", string3)
+list_of_strings = ["banana", "apple", "date", "cherry"]
+list_of_strings.sort()
+print("Sorted list of strings:", list_of_strings)
